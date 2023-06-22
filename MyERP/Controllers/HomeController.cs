698e0761d32372cb69906aabd85092ab4d536a17
rovinsharmagit <internetsharma45@gmail.com>
2023-06-22 19:05:34 +0530

    Configuring Login Controller

--- MyERP/Controllers/HomeController.cs
+++ MyERP/Controllers/HomeController.cs
@@ -8,6 +8,8 @@ namespace MyERP.Controllers
 {
     public class HomeController : Controller
     {
+         readonly db_class.EaseErpV1Entities _db = new db_class.EaseErpV1Entities();
+
         public ActionResult Index()
         {
             return View();
@@ -16,6 +18,37 @@ namespace MyERP.Controllers
         public ActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult LoginUSer(string email, string password)
+        {
+            var user = _db.tblUsers.Where(u => u.Email== email && u.Password == password).FirstOrDefault();
+            if (user != null) 
+            {
+                Session["UserTypeID"] = user.UserTypeID;
+                Session["FullName"] = user.FullName;
+                Session["Email"] = user.Email;
+                Session["ContactNo"] = user.ContactNo;
+                Session["UserName"] = user.UserName;
+                Session["Password"] = user.Password;
+                Session["IsActive"] = user.IsActive;  
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.Message = "Invalid Credentials!";
+
+                Session["UserTypeID"] = string.Empty;
+                Session["FullName"] = string.Empty;
+                Session["Email"] = string.Empty;
+                Session["ContactNo"] = string.Empty;
+                Session["UserName"] = string.Empty;
+                Session["Password"] = string.Empty;
+                Session["IsActive"] = string.Empty;
+            }
+
+            return View("Login");
         }
 
         public ActionResult ForgetPassword() 
